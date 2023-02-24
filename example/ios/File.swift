3f2a6d8f8731fe4358ea6476a65b69b5c87b0ec0
Adam Ivancza <adam.ivancza@gmail.com>
2023-02-24 17:09:59 +0100

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  ParselyReactNativeSdkExample
+//
+
+import Foundation
---
+++ ios/ParselyReactNativeSdk.swift
@@ -0,0 +1,8 @@
+@objc(ParselyReactNativeSdk)
+class ParselyReactNativeSdk: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
